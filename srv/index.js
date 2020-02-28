a249c0b3256ebc71e33d26f57e9845e216b0a7ce
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-02-28 10:44:54 +0900

    add game generating endpoint

--- srv/index.js
+++ srv/index.js
@@ -5,6 +5,36 @@ const knex = require("knex")(require("../knexfile.js"));
 
 // express set up
 const app = express();
+
+// generate a game
+app.get("/yopardy/game", (req, res) => {
+  async function generateGame(numberOfCategories, values) {
+    const categories = await knex("categories")
+      .select("id", "name")
+      .orderByRaw("RANDOM()")
+      .limit(numberOfCategories);
+    for (let i = 0; i < categories.length; i++) {
+      categories[i].questions = {};
+      for (let j = 0; j < values.length; j++) {
+        categories[i].questions[values[j]] = await knex("questions")
+          .select("question", "answer", "value")
+          .where("category_id", categories[i].id)
+          .where("value", values[j])
+          .orderByRaw("RANDOM()")
+          .first();
+      }
+    }
+    return categories;
+  }
+
+  // hardcoded currently but can pretty easily be changed with user input
+  const numberOfCategories = 5;
+  const values = [100, 200, 300, 400, 500];
+
+  generateGame(numberOfCategories, values).then(game => res.json(game));
+});
+
+// serve static folder
 app.use("/yopardy", express.static("build"));
 
 // let's go!
